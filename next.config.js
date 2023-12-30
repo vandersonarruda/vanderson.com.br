9d18e85f85a3c3d8ee9de7746bb828d281dcf5e6
Vanderson Arruda <contato@vanderson.com.br>
2023-12-30 12:12:38 -0300

    feat: add project mock data and add project route api

--- next.config.js
+++ next.config.js
@@ -1,4 +1,8 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  images: {
+    domains: ['flowbite.s3.amazonaws.com'],
+  },
+}
 
 module.exports = nextConfig
---
+++ src/app/api/projects/featured/route.ts
@@ -0,0 +1,7 @@
+import data from '../data.json'
+
+export async function GET() {
+  const featuredProjects = data.projects.filter((project) => project.featured)
+
+  return Response.json(featuredProjects)
+}
