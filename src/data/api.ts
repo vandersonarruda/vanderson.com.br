d587e749e6933936f0bd4b7f256d8535a37c2c6e
Vanderson Arruda <contato@vanderson.com.br>
2023-12-30 12:17:01 -0300

    fix: ensure API base URL and prefix

--- src/data/api.ts
+++ src/data/api.ts
@@ -3,7 +3,8 @@ import { env } from '@/env'
 // Using Fetch API to combine data with base URL
 export function api(path: string, init?: RequestInit) {
   const baseUrl = env.NEXT_PUBLIC_API_BASE_URL
-  const url = new URL(path, baseUrl)
+  const apiPrefix = '/api'
+  const url = new URL(apiPrefix.concat(path), baseUrl)
 
   return fetch(url, init)
 }
