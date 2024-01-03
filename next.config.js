4f0fe66078e78f7f57979ac9a1f7d2fe6048837d
Vanderson Arruda <contato@vanderson.com.br>
2024-01-03 19:29:21 -0300

    feat: ensure videoplayer and skeleton is correct

--- next.config.js
+++ next.config.js
@@ -2,6 +2,7 @@
 const nextConfig = {
   images: {
     domains: ['s3.us-east-2.amazonaws.com'],
+    // loader: 'custom',
   },
 }
 
--- src/components/skeleton.tsx
+++ src/components/skeleton.tsx
@@ -4,7 +4,7 @@ import { twMerge } from 'tailwind-merge'
 export function Skeleton({ className, ...props }: ComponentProps<'div'>) {
   return (
     <div
-      className={twMerge('animate-pulse rounded-md bg-zinc-50/10', className)}
+      className={twMerge('animate-pulse rounded-xl bg-zinc-50/10', className)}
       {...props}
     />
   )
