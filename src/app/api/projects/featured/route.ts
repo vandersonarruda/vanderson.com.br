c0c2b4a6c82b0f9e4f28ff6f9ce6e93d2ffce66f
Vanderson Arruda <contato@vanderson.com.br>
2024-01-10 14:57:41 -0300

    fix: refactor GET function in featured projects route

--- src/app/api/projects/featured/route.ts
+++ src/app/api/projects/featured/route.ts
@@ -1,7 +1,49 @@
 import data from '../../data.json'
 
 export async function GET() {
-  const featuredProjects = data.projects.filter((project) => project.featured)
+  try {
+    const featuredProjects = data.projects.filter((project) => project.featured)
 
-  return Response.json(featuredProjects)
+    if (!featuredProjects) {
+      return new Response(JSON.stringify({ message: 'Projects not found' }), {
+        status: 400,
+      })
+    }
+
+    return new Response(JSON.stringify(featuredProjects))
+  } catch (error) {
+    const message = error instanceof Error ? error.message : 'Unknown error'
+    return new Response(
+      JSON.stringify({ message: 'Error processing request', error: message }),
+      { status: 500 },
+    )
+  }
 }
+
+// import { z } from 'zod'
+// import data from '../../data.json'
+
+// export async function GET(
+//   _: Request,
+//   { params }: { params: { slug: string } },
+// ) {
+//   try {
+//     const slug = z.string().parse(params.slug)
+
+//     const project = data.projects.find((p) => p.slug === slug)
+
+//     if (!project) {
+//       return new Response(JSON.stringify({ message: 'Project not found' }), {
+//         status: 400,
+//       })
+//     }
+
+//     return new Response(JSON.stringify(project))
+//   } catch (error) {
+//     const message = error instanceof Error ? error.message : 'Unknown error'
+//     return new Response(
+//       JSON.stringify({ message: 'Error processing request', error: message }),
+//       { status: 500 },
+//     )
+//   }
+// }
