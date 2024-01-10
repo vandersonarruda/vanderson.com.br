7d6ffc17f5f2d9e01370eb055f7597586bc40410
Vanderson Arruda <contato@vanderson.com.br>
2024-01-10 12:04:59 -0300

    fix: refactor GET function in route.ts

--- src/app/api/projects/[slug]/route.ts
+++ src/app/api/projects/[slug]/route.ts
@@ -1,27 +1,39 @@
-import { z } from 'zod'
+// import { z } from 'zod'
+// import data from '../../data.json'
+
+// export async function GET(
+//   _: Request,
+//   { params }: { params: { slug: string } },
+// ) {
+//   try {
+//     const slug = z.string().parse(params.slug)
+
+//     const project = data.projects.find((p) => p.slug === slug)
+
+//     if (!project) {
+//       return new Response(JSON.stringify({ message: 'Project not found' }), {
+//         status: 400,
+//       })
+//     }
+
+//     return new Response(JSON.stringify(project))
+//   } catch (error) {
+//     const message = error instanceof Error ? error.message : 'Unknown error'
+//     return new Response(
+//       JSON.stringify({ message: 'Error processing request', error: message }),
+//       { status: 500 },
+//     )
+//   }
+// }
+
 import data from '../../data.json'
 
 export async function GET(
   _: Request,
   { params }: { params: { slug: string } },
 ) {
-  try {
-    const slug = z.string().parse(params.slug)
+  // const featuredProjects = data.projects.filter((project) => project.featured)
+  const project = data.projects.find((project) => project.slug === params.slug)
 
-    const project = data.projects.find((p) => p.slug === slug)
-
-    if (!project) {
-      return new Response(JSON.stringify({ message: 'Project not found' }), {
-        status: 400,
-      })
-    }
-
-    return new Response(JSON.stringify(project))
-  } catch (error) {
-    const message = error instanceof Error ? error.message : 'Unknown error'
-    return new Response(
-      JSON.stringify({ message: 'Error processing request', error: message }),
-      { status: 500 },
-    )
-  }
+  return Response.json(project)
 }
